6e27379a69070aad2067d287dc521212978450c8
judy-chelangat <judysigilai75@gmail.com>
2023-06-16 12:36:21 +0300

    updated the solution

--- solution2.js
+++ solution2.js
@@ -1,15 +1,28 @@
 //Challenge 2: Speed Detector (Toy Problem)
 //Write a program that takes as input the speed of a car e.g 80.
-const speedOfCar=prompt("please input the speed of car:")
+const speedOfCar =prompt("please input the speed of car:")
 
 //If the speed is less than 70, it should print “Ok”
 //Otherwise, for every 5 km/s above the speed limit (70), 
 //it should give the driver one demerit point and print the total number of demerit points.
 
+//if the speed is 80, it should print: “Points: 2”. If the driver gets more than 12 points, 
+//the function should print: “License suspended”.
+
+
 if(speedOfCar<70){
     alert("Ok")
 }
-else (speedOfCar>70){
-    const speedOver=(speedOfCar-70)/5
-    console.log("your demirit points is:" speedOver)
+else {
+   function speedOver(){    
+    let demerit=(speedOfCar-70)/5
+    if(demerit>12){
+        alert("License suspended")
+    }
+    else{
+        const message =`Points: ${demerit}`
+        alert(message)
+    }
+   }
+   speedOver()
 }
