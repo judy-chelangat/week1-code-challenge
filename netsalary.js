51d242f63bfc0b4b5336be7a10d7b80a954e5723
judy-chelangat <judysigilai75@gmail.com>
2023-06-17 22:55:31 +0300

    updated my netsalary.js file

--- netsalary.js
+++ netsalary.js
@@ -2,110 +2,115 @@
 //Write a program whose major task is to calculate an individual’s Net Salary 
 //by getting the inputs of basic salary and benefits
 const prompt = require("prompt-sync")();
-const basicSalary = prompt("enter your basic salary and benefits :")
+const basicSalary = prompt("enter your basic salary:")
+const benefits=prompt("enter your benefits:")
 
-
+ 
 //Calculate the payee (i.e. Tax), NHIFDeductions, NSSFDeductions, gross salary, and net salary. 
 
 // gross salary calculation
+const grossSalary=parseInt(basicSalary) + parseInt(benefits) // calculating the gross salary 
 function calculateGrossSalary() {
-   console.log(`Your gross salary is : ${basicSalary}`)
+
+   //console.log(`Your basic  salary is : ${basicSalary}`)
+   console.log(`Your gross salary is: ${grossSalary} `)
+
 }
 calculateGrossSalary()
 
 
 //NHIFDeductions 	calculations 
 function NHIFDeductions(){
-    if(basicSalary>=0 && basicSalary<=5999){
+    if(grossSalary>=0 && grossSalary<=5999){
         console.log(`your nhif deduction is: ${150}`)
     }
-    else if(basicSalary>=6000 && basicSalary<=7999){
+    else if(grossSalary>=6000 && grossSalary<=7999){
         console.log(`your nhif deduction is: ${300}`)
     }
-    else if(basicSalary>=8000 && basicSalary<=11999){
+    else if(grossSalary>=8000 && grossSalary<=11999){
         console.log(`your nhif deduction is: ${400}`)
     }
-    else if(basicSalary>=12000 && basicSalary<=14999){
+    else if(grossSalary>=12000 && grossSalary<=14999){
         console.log(`your nhif deduction is: ${500}`)
     }
-    else if(basicSalary>=15000 && basicSalary<=19999){
+    else if(grossSalary>=15000 && grossSalary<=19999){
         console.log(`your nhif deduction is: ${600}`)
     }
-    else if(basicSalary>=20000 && basicSalary<=24999){
+    else if(grossSalary>=20000 && grossSalary<=24999){
         console.log(`your nhif deduction is: ${750}`)
     }
-    else if(basicSalary>=25000 && basicSalary<=29999){
+    else if(grossSalary>=25000 && grossSalary<=29999){
         console.log(`your nhif deduction is: ${850}`)
     }
-    else if(basicSalary>=30000 && basicSalary<=34999){
+    else if(grossSalary>=30000 && grossSalary<=34999){
         console.log(`your nhif deduction is: ${900}`)
     }
-    else if(basicSalary>=35000 && basicSalary<=39999){
+    else if(grossSalary>=35000 && grossSalary<=39999){
         console.log(`your nhif deduction is: ${950}`)
     }
-    else if(basicSalary>=40000 && basicSalary<=44999){
+    else if(grossSalary>=40000 && grossSalary<=44999){
         console.log(`your nhif deduction is: ${1000}`)
     }
-    else if(basicSalary>=45000 && basicSalary<=49999){
+    else if(grossSalary>=45000 &&grossSalary<=49999){
         console.log(`your nhif deduction is: ${1100}`)
     }
-    else if(basicSalary>=50000 && basicSalary<=59999){
+    else if(grossSalary>=50000 && grossSalary<=59999){
         console.log(`your nhif deduction is: ${1200}`)
     }
-    else if(basicSalary>=60000 && basicSalary<=69999){
+    else if(grossSalary>=60000 && grossSalary<=69999){
         console.log(`your nhif deduction is: ${1300}`)
     }
-    else if(basicSalary>=70000 && basicSalary<=79999){
+    else if(grossSalary>=70000 && grossSalary<=79999){
         console.log(`your nhif deduction is: ${1400}`)
     }
-    else if(basicSalary>=80000 && basicSalary<=89999){
+    else if(grossSalary>=80000 && grossSalary<=89999){
         console.log(`your nhif deduction is: ${1500}`)
     }
-    else if(basicSalary>=90000 && basicSalary<=99999){
+    else if(grossSalary>=90000 && grossSalary<=99999){
         console.log(`your nhif deduction is: ${1600}`)
     }
-    else if(basicSalary>=100000){
+    else if(grossSalary>=100000){
         console.log(`your nhif deduction is: ${1700}`)
     }
 
 }
-NHIFDeductions()
+NHIFDeductions()  // calling the function
 
 //calculation of payee
      
      function payee()
      {
-       
-        if (basicSalary>=0 && basicSalary <= 24000)
+       let payee1=0
+        if (grossSalary>=0 && grossSalary <= 24000)
      {
-       const  payee1 = (0.1*basicSalary)
+         payee1 = (0.1*grossSalary)
        console.log(`your payee is: ${payee1}`)
     }
-    else if (basicSalary >=24001 && basicSalary <=32333) 
+    else if (grossSalary >=24001 && grossSalary <=32333) 
     {
-       const  payee2=((basicSalary) * 0.25)
-       console.log(`your payee is: ${payee2}`)
+        payee1=((grossSalary) * 0.25)
+       console.log(`your payee is: ${payee1}`)
     }
-    else if (basicSalary > 32333)
+    else if (grossSalary > 32333)
     {
-       const  payee3 = ((basicSalary) * 0.3)
-       console.log(`your payee is: ${payee3}`)
+        payee1 = ((grossSalary) * 0.3)
+       console.log(`your payee is: ${payee1}`)
     }
      }
-    payee()
+    payee() // calling the function payee
 
     // calculation of NSSF 
 
     function nssf1()
     {
         let nssf =0
-     if (basicSalary <= 6000)
+     if (grossSalary <= 6000)
      {
-        nssf = 0.06*basicSalary
-        console.logt(`your nssf is: ${nssf}`)
+        nssf = 0.06*grossSalary
+        console.log (`your nssf is: ${nssf}`)
      }
-     else if (basicSalary >=60001 && basicSalary<=18000) {
-        nssf = (basicSalary * 0.06)
+     else if (grossSalary >=60001 && grossSalary<=18000) {
+        nssf = (grossSalary * 0.06)
         console.log(`your nssf is: ${nssf}`)
      }
      else {
@@ -116,6 +121,6 @@ NHIFDeductions()
 
     // net pay
     
-    netSalary = basicSalary-(NHIFDeductions + nssf1 + payee)
+    netSalary = grossSalary- (NHIFDeductions - nssf1 - payee)
     console.log(`Your net salary is : ${netSalary}`);
     
